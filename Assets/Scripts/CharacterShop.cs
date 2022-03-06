4bbb4bd51db4bc08a1dc115dc0fa4479123556c1
wahyu <wahyu@game5mobile.com>
2022-03-06 10:25:49 +0700

    feat: Purchase & selecting

--- Assets/Scripts/CharacterShop.cs
+++ Assets/Scripts/CharacterShop.cs
@@ -1,45 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterShop : MonoBehaviour
 {
     [Header("Layout Settings")]
-    [SerializeField]
-    private float _itemSpacing = 0.5f;
 
-    private float _itemHeight;
+    [SerializeField] private float _itemSpacing = 0.5f;
 
     [Header("UI Elements")]
-    [SerializeField]
-    private Transform _shopItemsContainer;
 
-    [SerializeField]
-    private GameObject _itemPrefab;
+    [SerializeField] private Image _selectedCharacterIcon;
+    [SerializeField] private Transform _shopItemsContainer;
+    [SerializeField] private GameObject _itemPrefab;
 
     [Space(20f)]
-    [SerializeField]
-    private CharacterShopDatabase _database;
+
+    [SerializeField] private CharacterShopDatabase _database;
 
     [Header("Shop Events")]
-    [SerializeField]
-    private GameObject shopUI;
     
-    [SerializeField]
-    private Button buttonOpenShop;
+    [SerializeField] private GameObject shopUI;
+    [SerializeField] private Button buttonOpenShop;
+    [SerializeField] private Button buttonCloseShop;
+
+    [Space(20f)]
+    [Header("Main Menu")]
     
-    [SerializeField]
-    private Button buttonCloseShop;
+    [SerializeField] private Image _mainMenuCharacterImage;
+    [SerializeField] private TMP_Text _mainMenuCharacterName;
+
+    private float _itemHeight;
+    private int _newSelectedItemIndex = 0;
+    private int _previousSelectedItemIndex = 0;
 
     void Start()
     {
         AddShopEvents();
         GenerateShopItemsUI();
+        SetSelectedCharacter();
+
+        SelectItemUI(DataManager.GetSelectedCharacterIndex());
+
+        ChangePlayerSkin();
     }
 
-    private async void GenerateShopItemsUI()
+    private void SetSelectedCharacter()
     {
+        int index = DataManager.GetSelectedCharacterIndex();
+
+        DataManager.SetSelectedCharacter(_database.GetCharacter(index), index);
+    }
+
+    private void GenerateShopItemsUI()
+    {
+        MatchDatabasePurchasedCharactersWithDataManager();
+
         _itemHeight = _shopItemsContainer.GetChild(0).GetComponent<RectTransform>().sizeDelta.y;
         Destroy(_shopItemsContainer.GetChild(0).gameObject);
 
@@ -79,14 +95,78 @@ public class CharacterShop : MonoBehaviour
                 Vector2.up * ((_itemHeight + _itemSpacing) * count + _itemSpacing);
     }
 
+    private void MatchDatabasePurchasedCharactersWithDataManager()
+    {
+        int purchasedCharacterCount = DataManager.GetPurchasedCharacterIndexes().Count;
+
+        for (int i = 0; i < purchasedCharacterCount; i++)
+        {
+            int index = DataManager.GetPurchasedCharacter(i);
+            _database.PurchaseCharacter(index);
+        }
+    }
+
+    private void ChangePlayerSkin()
+    {
+        var character = DataManager.GetSelectedCharacter();
+
+        if (character.sprite != null)
+        {
+            _mainMenuCharacterImage.sprite = character.sprite;
+            _mainMenuCharacterName.text = character.name;
+
+            _selectedCharacterIcon.sprite = DataManager.GetSelectedCharacter().sprite;
+        }
+    }
+
     private void OnItemSelected(int index)
     {
-        Debug.Log("select " + index);
+        SelectItemUI(index);
+
+        DataManager.SetSelectedCharacter(_database.GetCharacter(index), index);
+
+        ChangePlayerSkin();
+    }
+
+    private void SelectItemUI(int index)
+    {
+        _previousSelectedItemIndex = _newSelectedItemIndex;
+        _newSelectedItemIndex = index;
+
+        var previousItem = GetItemUI(_previousSelectedItemIndex);
+        var newItem = GetItemUI(_newSelectedItemIndex);
+
+        previousItem.DeselectItem();
+        newItem.SelectItem();
+    }
+
+    private CharacterItem GetItemUI(int index)
+    {
+        return _shopItemsContainer.GetChild(index).GetComponent<CharacterItem>();
     }
 
     private void OnItemPurchased(int index)
     {
-        Debug.Log("purchase " + index);
+        var character = _database.GetCharacter(index);
+        var uiItem = GetItemUI(index);
+
+        if (DataManager.CanSpendCoins(character.price))
+        {
+            DataManager.SpendCoins(character.price);
+
+            SharedUI.instance.UpdateCoinsUITexts();
+
+            _database.PurchaseCharacter(index);
+
+            uiItem.SetAsPurchased();
+            uiItem.OnSelect(index, OnItemSelected);
+
+            DataManager.AddPurchasedCharacterIndex(index);
+        }
+        else
+        {
+            Debug.Log("Not enough coins!");
+        }
     }
 
     private void AddShopEvents()
